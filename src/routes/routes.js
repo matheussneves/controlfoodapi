61a5fb2153b0d3129d4df3d4ad0f60933f8e5ab8
Matheus Solano Dos Santos Neves <matheussneves@hotmail.com>
2024-11-15 04:41:49 -0300

    ed

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -47,5 +47,27 @@ router.get('/clientes/:id', TaskController.listarUmCliente);
 router.put('/clientes/:id', TaskController.atualizarCliente);
 router.delete('/clientes/:id', TaskController.removerCliente);
 
+// Rotas para entregadores
+router.post('/entregadores', TaskController.novoEntregador);
+router.get('/entregadores', TaskController.listarEntregadores);
+router.get('/entregadores/:id', TaskController.listarUmEntregador);
+router.put('/entregadores/:id', TaskController.atualizarEntregador);
+router.delete('/entregadores/:id', TaskController.removerEntregador);
+
+// Rotas para entregas
+router.post('/entregas', TaskController.novaEntrega);
+router.get('/entregas', TaskController.listarEntregas);
+router.get('/entregas/:id', TaskController.listarUmaEntrega);
+router.put('/entregas/:id', TaskController.atualizarEntrega);
+router.delete('/entregas/:id', TaskController.removerEntrega);
+
+// Rotas para pedidos
+router.post('/pedidos', TaskController.novoPedido);
+router.get('/pedidos', TaskController.listarPedidos);
+router.get('/pedidos/:id', TaskController.listarUmPedido);
+router.put('/pedidos/:id', TaskController.atualizarPedido);
+router.delete('/pedidos/:id', TaskController.removerPedido);
+
+
 // Exporte as rotas
 module.exports = router;
